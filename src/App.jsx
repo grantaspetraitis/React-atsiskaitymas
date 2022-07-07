787c9ce62ceba8edc52c4d0ce4905eeffffd335d
grantaspetraitis <grantas157@gmail.com>
2022-07-07 08:47:51 +0300

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Add from './pages/Add';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
     </Router>
   );
 }
