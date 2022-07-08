16b19f0d594fa20316d2fd71d3ed59a72c9a3012
grantaspetraitis <grantas157@gmail.com>
2022-07-08 09:58:01 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppContext } from '../Context';
+import { useContext, useEffect } from 'react';
 
 const Navbar = () => {
 
-    
+    const navigate = useNavigate();
+
+    const logout = () => {
+        setLogin(null);
+        navigate("/login");
+      };
+
+    const { login, setLogin } = useContext(AppContext);
 
     return (
         <nav>
             <h1>Logo</h1>
             <ul className="navbar">
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-                <li>
-                    <Link to='/register'>Register</Link>
-                </li>
+                {login ? (
+                    <>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/add">Add</Link>
+                        </li>
+                        <li>
+                            <span style={{ cursor: "pointer" }} onClick={logout}>
+                                Log out
+                            </span>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     );
 }
- 
+
 export default Navbar;
